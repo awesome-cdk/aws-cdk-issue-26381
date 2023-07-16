106d4ee658a5d3ffde7d30738c217ace5e18f866
Dzhuneyt <dzhuneyt@dzhuneyt.com>
2023-07-16 08:39:33 +0300

    feat: Minimum reproducable setup

---
+++ packages/cdk/lib/cdk-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import path = require("path");
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    new NodejsFunction(this, "NodejsFunction", {
+      entry: path.resolve(__dirname, "lambda.ts"),
+    });
+  }
+}
